4c4001dec78828c1f639e8fe663a141e2a5dc842
dojx <dojx56@gmail.com>
2024-02-18 22:03:58 -0600

    more problems

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -18,7 +18,30 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        ListNode *res = new ListNode(0);
+        ListNode *temp = res;
+        int sum, carry = 0, val;
+        while (l1 != nullptr || l2 != nullptr) {
+            sum = carry;
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            carry = sum / 10; 
+            val = sum % 10;
+            ListNode *new_node = new ListNode(val);
+            temp->next = new_node;
+            temp = temp->next;
+        }
+        if (carry) {
+            ListNode *new_node = new ListNode(1);
+            temp->next = new_node;
+        }
+        return res->next;
     }
 };
 // @lc code=end
