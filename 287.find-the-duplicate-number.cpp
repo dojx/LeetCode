7e2c5468dc00788d2978ac6669ed58177d7d8e41
dojx <dojx56@gmail.com>
2024-02-20 19:29:29 -0600

    287-find-the-duplicate

--- 287.find-the-duplicate-number.cpp
+++ 287.find-the-duplicate-number.cpp
@@ -8,7 +8,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        
+        int s = 0, f = 0;
+
+        // find the start of loop
+        while (s == 0 || s != f) {
+            s = nums[s];
+            f = nums[nums[f]];
+        }
+
+        // floyds stupid algorithm
+        s = 0;
+        while (s != f) {
+            s = nums[s];
+            f = nums[f];
+        }
+
+        return s;
     }
 };
 // @lc code=end
